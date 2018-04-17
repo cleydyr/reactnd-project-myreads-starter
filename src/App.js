3aeb9f1bb8d45a8a2b5e3105ef755e59cb620b1f
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-17 19:47:40 -0300

    props.author

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ const BookItem = props => (
 				</div>
 			</div>
 			<div className="book-title">{props.title}</div>
-			<div className="book-authors">Harper Lee</div>
+			<div className="book-authors">{props.author}</div>
 		</div>
 	</li>
 );
@@ -70,6 +70,7 @@ class BooksApp extends React.Component {
                     <ol className="books-grid">
 					  <BookItem style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} 
 					  	title="To Kill a Mockingbird"
+						author="Harper Lee"
 					  />
                       <li>
                         <div className="book">
