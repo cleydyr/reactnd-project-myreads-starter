b76f113bbfd6fc7498387bd62c2b0c70df0c4f73
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-17 19:46:47 -0300

    props.title

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const BookItem = props => (
 					</select>
 				</div>
 			</div>
-			<div className="book-title">To Kill a Mockingbird</div>
+			<div className="book-title">{props.title}</div>
 			<div className="book-authors">Harper Lee</div>
 		</div>
 	</li>
@@ -68,7 +68,9 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-					  <BookItem style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} />
+					  <BookItem style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }} 
+					  	title="To Kill a Mockingbird"
+					  />
                       <li>
                         <div className="book">
                           <div className="book-top">
