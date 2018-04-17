a04377f19c945673b94d419d8946ad52fee2b326
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-17 20:27:02 -0300

    Apply

--- src/App.js
+++ src/App.js
@@ -2,27 +2,7 @@ import React from 'react'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Title from './components/Title';
-
-const BookItem = props => (
-	<li>
-		<div className="book">
-			<div className="book-top">
-				<div className="book-cover" style={{backgroundImage : `url(${props.image})`, ...props.style}}></div>
-				<div className="book-shelf-changer">
-					<select>
-						<option value="none" disabled>Move to...</option>
-						<option value="currentlyReading">Currently Reading</option>
-						<option value="wantToRead">Want to Read</option>
-						<option value="read">Read</option>
-						<option value="none">None</option>
-					</select>
-				</div>
-			</div>
-			<div className="book-title">{props.title}</div>
-			<div className="book-authors">{props.author}</div>
-		</div>
-	</li>
-);
+import BookItem from './components/BookItem';
 
 class BooksApp extends React.Component {
   state = {
