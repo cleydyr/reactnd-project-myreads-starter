fc1e4bcea87f1dcf532c4eab365fc3cd3e2f2d30
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-18 20:16:33 -0300

    Uncomment necessary import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 import Title from './components/Title';
