1fafcdb986cb499f9ae4afca34c778b38ef0f0f9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-18 20:00:24 -0300

    Extract component to new file

--- src/App.js
+++ src/App.js
@@ -6,21 +6,8 @@ import './App.css'
 
 import Title from './components/Title';
 import BookContent from './components/BookContent';
-import SearchCloseButton from './components/SearchCloseButton'
-
-const SearchInput = props => (
-	<div className="search-books-input-wrapper">
-		{/*
-			NOTES: The search from BooksAPI is limited to a particular set of search terms.
-			You can find these search terms here:
-			https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-			However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-			you don't find a specific author or title. Every search is limited by search terms.
-		*/}
-		<input type="text" placeholder="Search by title or author" onChange={props.onChange} value={props.query} />
-	</div>
-);
+import SearchCloseButton from './components/SearchCloseButton';
+import SearchInput from './components/SearchInput';
 
 const Search = props => (
 	<div className="search-books">
