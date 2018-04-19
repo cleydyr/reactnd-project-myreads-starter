f32dde443823e15d08484602e964e6b7e8c1857d
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-04-19 20:01:40 -0300

    Extract component to new file

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+
+import BookshelfChanger from './BookshelfChanger';
+
 import '../App.css';
 
 export default class BookItem extends Component {
@@ -8,15 +11,10 @@ export default class BookItem extends Component {
 				<div className="book">
 					<div className="book-top">
 						<div className="book-cover" style={{backgroundImage : `url(${this.props.image})`, ...this.props.style}}></div>
-						<div className="book-shelf-changer">
-							<select>
-								<option value="none" disabled>Move to...</option>
-								<option value="currentlyReading">Currently Reading</option>
-								<option value="wantToRead">Want to Read</option>
-								<option value="read">Read</option>
-								<option value="none">None</option>
-							</select>
-						</div>
+						<BookshelfChanger
+							current={this.props.currentBookshelf}
+							onChange={({target}) => this.props.onChange(target.value)}
+						/>
 					</div>
 					<div className="book-title">{this.props.title}</div>
 					{this.props.authors.map(author => <div className="book-authors">{author}</div>)}
